870b9590f4d596011287f73393ab87a31bc98fdb
mohammad eydi <mohammadeydi2020@gmail.com>
2026-02-24 12:29:39 +0330

    feat: Add CHANGELOG.txt to track project changes and update next.config.mjs.

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        // در حالت dev، بهینه‌سازی را غیرفعال کن تا به IP لوکال گیر ندهد
+        unoptimized: process.env.NODE_ENV === 'development', 
+        
         remotePatterns: [
             {
-                protocol: "https",
-                hostname: "picsum.photos",
+                protocol: "http",
+                hostname: "127.0.0.1",
+                port: "1337",
+                pathname: "/uploads/**",
             },
             {
                 protocol: "http",
                 hostname: "localhost",
                 port: "1337",
+                pathname: "/uploads/**",
             },
             {
-                protocol: 'http',
-                hostname: '127.0.0.1',
-                port: '1337',
+                protocol: "https",
+                hostname: "picsum.photos",
             }
         ],
-        qualities: [50, 75, 90, 100],
     },
 };
 
